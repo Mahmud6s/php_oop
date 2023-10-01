d77106acb2de8a35d4d39fb283769d61da10d66a
mahmud <mahmud955556@gmail.com>
2023-10-01 20:56:56 +0600

    new added

---
+++ abstaction/reservation.php
@@ -0,0 +1,12 @@
+<?php
+
+class reservation{
+    private  $host = "host class";
+    private $database = "database class";
+
+    public function cancle(){
+        
+    }
+
+}
+$reservation = new reservation();
