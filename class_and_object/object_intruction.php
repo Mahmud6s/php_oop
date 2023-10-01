772c4cde990f545e7386f675e7cab9e7f952a1a9
mahmud <mahmud955556@gmail.com>
2023-10-01 18:26:27 +0600

    intruction added

--- class_and_object/object_intruction.php
+++ class_and_object/object_intruction.php
@@ -1,2 +1,26 @@
 <?php
 
+class Song{
+    public $songId;
+    public $songTitle;
+}
+
+$numb = new Song();
+$numb->songId = 1;
+$numb->songTitle = 'balam';
+
+
+
+class Playlist {
+    public $name;
+    public $songs = [];
+
+    public function addSong($song){
+        $this->songs[] = $song;
+    }
+}
+
+$RockPlaylist = new Playlist();
+$RockPlaylist->name = 'Rock';
+$RockPlaylist->addSong($numb);
+print_r($RockPlaylist);
