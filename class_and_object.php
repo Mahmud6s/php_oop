f3c12c803146854f34f5e1d7f7d860aa419ae8ac
mahmud <mahmud955556@gmail.com>
2023-10-01 17:56:46 +0600

    class added

--- class_and_object.php
+++ class_and_object.php
@@ -15,3 +15,22 @@ echo "<br>";
 echo $user->name;
 
 echo "<br>"; //
+
+
+class Cart{
+    public $itemsTotal;
+    public $ShippingCost;
+    public $discount;
+
+    public function Calculate(){
+        $subtotal = $this->itemsTotal + $this->ShippingCost - $this->discount;
+        return $subtotal;
+    }
+
+}
+
+$cart = new Cart();
+$cart->itemsTotal = 100;
+$cart->ShippingCost = 10;
+$cart->discount = 10;
+echo $cart->Calculate();
